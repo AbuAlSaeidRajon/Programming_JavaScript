cff54cc89b05dfeb5468088c11a5ced54298c272
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:21:51 +0200

    ex-5 done

---
+++ JS_Exxercise/exercise_05.js
@@ -0,0 +1,10 @@
+function getLastCharacter(name) {
+    // Use indexing with length - 1 to get the last character of the name
+    return name[name.length - 1];
+  }
+  
+  // Sample usage - do not modify
+  console.log(getLastCharacter("Sam")); // "m"
+  console.log(getLastCharacter("Alex")); // "x"
+  console.log(getLastCharacter("Charley")); // "y"
+  
