377cff75a215ba0b69e25c48d98f6c7dec987a7a
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:29:53 +0200

    ex-5 added

---
+++ JS_Array/JS_Exercise_1/Exercise_05.js
@@ -0,0 +1,7 @@
+// Exercise 5: Append a new member "Linda" to the end of the array.
+// Write your code here:
+
+const teamMembers = ["John", "Alex", "Mike", "Bobb"];
+
+teamMembers.splice(4, 0, "Linda");
+console.log(teamMembers);
