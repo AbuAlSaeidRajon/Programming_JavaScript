889042bea7fc05072e3f9773d626d2226e14645b
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:08:48 +0200

    text length 2 completed

---
+++ Week_3/text_length_2.js
@@ -0,0 +1,9 @@
+//use .length to find the number of characters in a string
+
+let text = "Hello World";
+console.log(text.length);
+
+//String Interpolation
+//String interpolation allows you to embed expressions, like variables, within a string. This is achieved using ${expression} syntax:
+let language = `JavaScript`;
+`I am learning ${language}`;
