ecb9600fb53d92a61e74e0eb1c585b26ff86715c
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:35:24 +0200

    ex-14 added

---
+++ JS_Array/JS_Exercise_1/Exercise_14.js
@@ -0,0 +1,8 @@
+// Exercise 14: Transforming members with `map` to be all written uppercase
+// Write your code here:
+
+const teamMembers = ["John", "Mike", "Alex", "Bobb"];
+
+const teamMembersUppercase = (teamMembers.map(member => member.toUpperCase()));
+
+console.log(teamMembersUppercase);
