22407f69dcd09bbb5a959fa78910b1cad9fc0b0b
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:28:57 +0200

    ex-3 added

---
+++ JS_Array/JS_Exercise_1/Exercise_03.js
@@ -0,0 +1,7 @@
+// Exercise 3: Remove the last member of the array.
+// Write your code here:
+
+const teamMembers = ["John", "Alex", "Mike", "Bobb"];
+
+teamMembers.pop("Bobb");
+console.log(teamMembers);
