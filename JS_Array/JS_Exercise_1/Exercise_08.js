98443fd2c5f1b0d64b9f070c7954a66c234d31b4
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:31:31 +0200

    ex-8 added

---
+++ JS_Array/JS_Exercise_1/Exercise_08.js
@@ -0,0 +1,6 @@
+// Exercise 8: Try to find the index of "Jake" (who is not in the array).
+// Write your code here:
+
+const teamMembers = ["John", "Alex", "Mike", "Bobb"];
+
+console.log(teamMembers.indexOf("Jake"));
