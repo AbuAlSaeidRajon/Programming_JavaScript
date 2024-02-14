2484870582f2022ba0e8a8ec091c32001f9a69b1
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:07:03 +0200

    basic dom1 added

---
+++ Basic_Dom/basic_dom1/script.js
@@ -0,0 +1,12 @@
+/* 
+
+Write JavaScript code that selects content id by using getElementById.
+Write a function which will update the text content of the selected element to "Hello, World!" by clicking the button.
+*/
+
+function updateContent() {
+    const myElement = document.getElementById("myContent");
+  
+    myElement.textContent = "Hello World!";
+  }
+  
