81dbdcc34f100f8a9da795530f776af95878755c
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:32:46 +0200

    con-8 done

---
+++ JS_Conditions/conditions_08.js
@@ -0,0 +1,20 @@
+const age = 26;
+const name = "Rajon";
+const isStudent = true;
+const hobbies = ["Sports", "Cricekt", "Badminton"];
+const info = { schoolName: "BCH", grade: 5 };
+
+console.log(age, name, isStudent, hobbies, info);
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+console.log(typeof hobbies);
+console.log(typeof info);
+
+// another task
+
+let a = 12;
+let b = 24;
+
+const sum = a + b;
+console.log(sum);
