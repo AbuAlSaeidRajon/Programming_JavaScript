2be9d82182c5ee59a746333db21304a374ecf775
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:17:39 +0200

    exercise-2 done

---
+++ JS_Exxercise/exercise_02.js
@@ -0,0 +1,10 @@
+function shoutMyName(name) {
+    // Use the toUpperCase method to convert the name to uppercase
+    return name.toUpperCase();
+  }
+  
+  // Sample usage - do not modify
+  console.log(shoutMyName("Sam")); // "SAM"
+  console.log(shoutMyName("Charley")); // "CHARLEY"
+  console.log(shoutMyName("alex")); // "ALEX"
+  
