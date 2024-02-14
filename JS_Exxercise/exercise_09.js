a990a7ccff42960e6fe3562356064fc0cafe8ff3
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:24:02 +0200

    ex-9 done

---
+++ JS_Exxercise/exercise_09.js
@@ -0,0 +1,9 @@
+function getFullName(firstName, lastName) {
+    // Use string interpolation with template literals
+    return `${firstName} ${lastName}`;
+  }
+  
+  // Sample usage - do not modify
+  console.log(getFullName("Sam", "Doe")); // "Sam Doe"
+  console.log(getFullName("Alex", "Blue")); // "Alex Blue"
+  
