c43e3d1252338de0c84c90aa0e7224a702b0d9af
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:32:37 +0200

    ex-10 added

---
+++ JS_Array/JS_Exercise_1/Exercise_10.js
@@ -0,0 +1,9 @@
+// Exercise 10: Concatenate a new member "Bob" to the end of the array and create a new array.
+// Write your code here:
+
+const teamMembers1 = ["John", "Mike", "Alex"];
+const teamMembers2 = ["Bob"];
+
+const friends = teamMembers1.concat(teamMembers2);
+
+console.log(friends);
