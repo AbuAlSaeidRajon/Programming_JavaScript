b97cfee18cb9a796ff360030c5e7de16b003fe5b
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:25:43 +0200

    ex-1 done

---
+++ JS_Array/JS_Exercise_1/Exercise_01.js
@@ -0,0 +1,7 @@
+// Exercise 1: Loop through `teamMembers` and log each name to the console.
+// Write your code here:
+
+const teamMembers = ["John", "Alex", "Mike", "Bobb"];
+for (let i = 0; i < teamMembers.length; i++) {
+  console.log(teamMembers[i]);
+}
