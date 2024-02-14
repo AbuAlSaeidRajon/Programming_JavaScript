5972f713fddae31da13f786f72cdfd57c9dee091
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:30:28 +0200

    ex-6 added

---
+++ JS_Array/JS_Exercise_1/Exercise_06.js
@@ -0,0 +1,7 @@
+// Exercise 6: Create a new array that excludes the first two members.
+// Write your code here:
+
+const teamMembers = ["John", "Alex", "Mike", "Bobb"];
+
+teamMembers.splice(0, 2);
+console.log(teamMembers);
