bebf53dfb4281062c06815e409ac166f706852f7
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:23:34 +0200

    ex-8 done

---
+++ JS_Exxercise/exercise_08.js
@@ -0,0 +1,9 @@
+function sayHello(name) {
+    // Concatenate the greeting with the name
+    return "Hello " + name;
+  }
+  
+  // Sample usage - do not modify
+  console.log(sayHello("Alex")); // "Hello Alex"
+  console.log(sayHello("Sam")); // "Hello Sam"
+  
