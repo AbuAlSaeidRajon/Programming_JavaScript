15c6e0154a8b596e53ebacd8eaf577ab69721f12
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:15:56 +0200

    exercise 1 done

---
+++ JS_Exxercise/exercise_01.js
@@ -0,0 +1,10 @@
+function getCharCount(str) {
+    // Use the length property of the string to get the count of characters
+    return str.length;
+  }
+  
+  // Sample usage - do not modify
+  console.log(getCharCount("Sam")); // 3
+  console.log(getCharCount("Alex 123")); // 8
+  console.log(getCharCount("Charley is here")); // 15
+  
