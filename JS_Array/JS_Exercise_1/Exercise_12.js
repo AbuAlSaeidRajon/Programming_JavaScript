3797aa7416a1034407521a84043accc0bcf66d6f
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:33:52 +0200

    ex-12 added

---
+++ JS_Array/JS_Exercise_1/Exercise_12.js
@@ -0,0 +1,10 @@
+// Exercise 12: Combining Arrays with `concat`
+// Assume `newMembers` array is defined
+// Write your code here:
+
+const teamMembers = ["John", "Mike", "Alex", "Bobb"];
+let newMembers = ['Tina', 'Dean'];
+
+const newTeamMembers = teamMembers.concat(newMembers);
+
+console.log(newTeamMembers);
