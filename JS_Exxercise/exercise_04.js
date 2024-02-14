44f5b23c5ac731c40306140dccbfd3693f582f52
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:18:51 +0200

    ex-4 done

---
+++ JS_Exxercise/exercise_04.js
@@ -0,0 +1,9 @@
+function getFirstCharacter(name) {
+    // Use indexing [0] to get the first character of the name
+    return name[0];
+  }
+  
+  // Sample usage - do not modify
+  console.log(getFirstCharacter("Amsterdam")); // "A"
+  console.log(getFirstCharacter("Japan")); // "J"
+  
