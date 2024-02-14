fd2626eb4f8d844cbf5cd50b249cb8508cdec7bd
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:29:27 +0200

    ex-4 added

---
+++ JS_Array/JS_Exercise_1/Exercise_04.js
@@ -0,0 +1,7 @@
+// Exercise 4: Add a new member "Alex" to the front of the array.
+// Write your code here:
+
+const teamMembers = ["John", "Mike", "Bobb"];
+
+teamMembers.unshift("Alex");
+console.log(teamMembers);
