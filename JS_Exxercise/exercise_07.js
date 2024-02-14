a546fdcac303fc8c1aed79aa7c29501bbe98df07
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:22:56 +0200

    ex-7 done

---
+++ JS_Exxercise/exercise_07.js
@@ -0,0 +1,9 @@
+function concatInitials(firstNameInitial, lastNameInitial) {
+    // Concatenate the firstNameInitial and lastNameInitial
+    return firstNameInitial + lastNameInitial;
+  }
+  
+  // Sample usage - do not modify
+  console.log(concatInitials("J", "D")); // "JD"
+  console.log(concatInitials("S", "B")); // "SB"
+  
