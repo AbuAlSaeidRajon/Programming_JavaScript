d7bf458ddea7555269ab21a0ca8d8af392109685
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:28:21 +0200

    con-3 done

---
+++ JS_Conditions/conditions_03.js
@@ -0,0 +1,14 @@
+function evenOrOdd(number) {
+    // Check if the number is even
+    if (number % 2 === 0) {
+      return "even";
+    } else {
+      return "odd";
+    }
+  }
+  
+  // Sample usage - do not modify
+  console.log(evenOrOdd(25)); // "odd"
+  console.log(evenOrOdd(18)); // "even"
+  console.log(evenOrOdd(-17)); // "odd"
+  
