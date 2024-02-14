cebe47745859e65828d8c01b14f3029a4ef3471a
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:18:22 +0200

    exercise-3 done

---
+++ JS_Exxercise/exercise_03.js
@@ -0,0 +1,9 @@
+function lowerName(name) {
+    // Use the toLowerCase method to convert the name to lowercase
+    return name.toLowerCase();
+  }
+  
+  // Sample usage - do not modify
+  console.log(lowerName("Sam")); // "sam"
+  console.log(lowerName("ALEX")); // "alex"
+  
