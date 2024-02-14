456135b50608839067a6042241ba4ad021d24a4c
Saeid Md <khanrajon5@gmail.com>
2024-02-14 16:28:49 +0200

    con-4 done

---
+++ JS_Conditions/conditions_04.js
@@ -0,0 +1,9 @@
+function greaterNum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+  }
+  
+  // Sample usage - do not modify
+  console.log(greaterNum(5, 10)); // 10
+  console.log(greaterNum(2, 1)); // 2
+  console.log(greaterNum(4, 25)); // 25
+  
