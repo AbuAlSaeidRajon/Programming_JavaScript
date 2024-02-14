c1991bd4691fa03ad7ffcbe343064760689cca69
Saeid Md <khanrajon5@gmail.com>
2024-02-14 17:28:11 +0200

    ex-e done

---
+++ JS_Array/JS_Exercise_1/Exercise_02.js
@@ -0,0 +1,7 @@
+// Exercise 2: Remove the first member of the array.
+// Write your code here:
+
+const teamMembers = ["John", "Alex", "Mike", "Bobb"];
+
+teamMembers.pop("John");
+console.log(teamMembers);
