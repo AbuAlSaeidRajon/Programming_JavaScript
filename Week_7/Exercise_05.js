dbdaada0a35beb1d267f4b505a689a9ffc05e506
Saeid Md <khanrajon5@gmail.com>
2024-02-26 14:05:53 +0200

    exercise_5 complted

---
+++ Week_7/Exercise_05.js
@@ -0,0 +1,17 @@
+// Task 5: Function with Try-Catch
+/*
+Create a function that uses try-catch to handle an array index out of bounds error.
+*/
+
+function task5(array, index) {
+    // Code here
+    try {
+        if (index < 0 || index >= array.length) {
+            throw new Error("Index out of bounds");
+        }
+        console.log("Value:", array[index]);
+    } catch (error) {
+        console.error(error.message);
+    }
+  }
+  task5();
