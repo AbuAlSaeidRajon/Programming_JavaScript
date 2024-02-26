de532e3437bd0ce4764dd1460efe5783e3b10021
Saeid Md <khanrajon5@gmail.com>
2024-02-26 14:38:11 +0200

    cars class added

---
+++ Cars/script.js
@@ -0,0 +1,11 @@
+class Car {
+    constructor(licensePlate, maker, model, owner, price, color) {
+      this.licensePlate = licensePlate;
+      this.maker = maker;
+      this.model = model;
+      this.owner = owner;
+      this.price = price;
+      this.color = color;
+    }
+  }
+  
