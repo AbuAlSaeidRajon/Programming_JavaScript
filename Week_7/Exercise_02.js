e3e232f0837ceca1303efec7d26d3568e623726a
Saeid Md <khanrajon5@gmail.com>
2024-02-26 13:10:10 +0200

    exercise_2 added

---
+++ Week_7/Exercise_02.js
@@ -0,0 +1,10 @@
+// Task 2: Catching ReferenceError
+/*
+Attempt to access an undefined variable inside a try block.
+Catch the ReferenceError and log it to the console.
+*/
+
+function task2() {
+    // Code here
+  }
+  
