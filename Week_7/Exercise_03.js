d59b3e4ab8de6989a7f6b7e5553e03023e0c356c
Saeid Md <khanrajon5@gmail.com>
2024-02-26 13:42:34 +0200

    exercise_3 completed

--- Week_7/Exercise_03.js
+++ Week_7/Exercise_03.js
@@ -5,4 +5,13 @@ Use a try-catch-finally block. In the finally block, log 'Execution completed'.
 
 function task3() {
     // Code here
+    try {
+        console,log("Execution started");
+    } catch (error) {
+        console.error('Error:', error.message);
+    } finally {
+        console.log("Execution Completed");
+    }
   }
+
+  task3();
