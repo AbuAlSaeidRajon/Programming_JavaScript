24e33fd8ef76feb498f72159eb304d30338d1710
Saeid Md <khanrajon5@gmail.com>
2024-02-26 13:12:37 +0200

    exercisr_2 done

--- Week_7/Exercise_02.js
+++ Week_7/Exercise_02.js
@@ -6,5 +6,12 @@ Catch the ReferenceError and log it to the console.
 
 function task2() {
     // Code here
+    try {
+        console.log(undefinedVariable);
+    } catch (error) {
+        console.log('ReferenceError:', error.message);
+    }
   }
+  
+  task2();
   
