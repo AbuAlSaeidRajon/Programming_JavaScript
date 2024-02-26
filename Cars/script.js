28790490fb0b91c742d28427d645d3774e292f28
Saeid Md <khanrajon5@gmail.com>
2024-02-26 14:39:23 +0200

    value added

--- Cars/script.js
+++ Cars/script.js
@@ -8,4 +8,21 @@ class Car {
       this.color = color;
     }
   }
+  const carArray = [];
+  
+  const carForm = document.getElementById('car-form');
+  carForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const licensePlate = document.getElementById('license-plate').value;
+    const maker = document.getElementById('maker').value;
+    const model = document.getElementById('model').value;
+    const owner = document.getElementById('owner').value;
+    const price = document.getElementById('price').value;
+    const color = document.getElementById('color').value;
+    const newCar = new Car(licensePlate, maker, model, owner, price, color);
+    carArray.push(newCar);
+    displayCars();
+    carForm.reset();
+  });
+  
   
