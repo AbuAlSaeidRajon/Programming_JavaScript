e3a7dccedf5c55cba67e19a801aaff59f89abbc1
Saeid Md <khanrajon5@gmail.com>
2024-03-15 12:20:44 +0200

    mapping done

--- Json_File/Pokémon/script.js
+++ Json_File/Pokémon/script.js
@@ -1,22 +1,27 @@
 let pokemons = [];
 
 const fetchData = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=500&offset=0')
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=500&offset=0")
     .then((response) => response.json())
     .then((json) => {
+      const fetches = json.results.map((item) => {
+        return fetch(item.url).then(res.json());
+      });
+
       pokemons = json.results;
       displayData(pokemons);
-    }).catch((error) => console.error("Error fetching data:", error));
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 };
 
 fetchData();
 
 const displayData = (data) => {
-  const container = document.querySelector('.data');
-  container.innerHTML = '';
+  const container = document.querySelector(".data");
+  container.innerHTML = "";
 
   data.forEach((pokemon) => {
-    const pokemonCard = document.createElement('div');
+    const pokemonCard = document.createElement("div");
     pokemonCard.innerHTML = `<h2>${pokemon.name}</h2>`;
     container.appendChild(pokemonCard);
   });
@@ -28,7 +33,7 @@ const debounce = (func, delay) => {
     const context = this;
     const args = arguments;
     clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(() => func.apply(context, args), delay);  
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
   };
 };
 
@@ -42,13 +47,13 @@ const searchFoxes = (e) => {
 searchInput.addEventListener('input', searchFoxes); */
 
 const searchPokemons = debounce((searchInput) => {
-/* console.log(searchInpit); to check what data we are getting in console */
+  /* console.log(searchInpit); to check what data we are getting in console */
   const filteredData = pokemons.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(searchInput.toLowerCase())
   );
   displayData(filteredData);
-}, 300); 
+}, 300);
 
-document.querySelector('#search').addEventListener('input', (e) => {
+document.querySelector("#search").addEventListener("input", (e) => {
   searchPokemons(e.target.value);
 });
